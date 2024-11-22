4ccaf6cf86cb84133b6ae3f66da8448bf95c8978
antonisoaho <isoahoanton@gmail.com>
2024-11-22 21:52:33 +0100

    setup global.css and navigation/routing

---
+++ src/app/(sub pages)/projects/page.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import bg from "../../../../public/background/projects-background.png";
+
+export default function Home() {
+  return (
+    <>
+      <Image
+        src={bg}
+        alt="Next.js Portfolio website's about page background image"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
+        priority
+        sizes="100vw"
+      />
+
+      {/* Render projectslist - make database integration */}
+    </>
+  );
+}
