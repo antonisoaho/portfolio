a9d1f4e310202cb349b1f09b8481cf0008f8a670
antonisoaho <anton@isoaho.se>
2026-06-22 22:58:55 +0200

    Replace Next.js portfolio with minimal Vite + React site.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div id="wrapper">
+      <h1>
+        anton<strong> isoaho</strong>.
+      </h1>
+      <p>
+        <a href="https://hitai.se">hitai.se</a>
+      </p>
+    </div>
+  );
+}
--- src/sanity/schemaTypes/index.js
+++
@@ -1,8 +0,0 @@
-import { blockContentType } from "./blockContentType";
-import { categoryType } from "./categoryType";
-import { projectType } from "./projectType";
-import { homeWorkType } from "./homeWorkType";
-
-export const schema = {
-  types: [blockContentType, categoryType, projectType, homeWorkType],
-};
